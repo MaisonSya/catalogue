1b5e95c390b3fe46694822351e3d0a6ee2b9b210
MaisonSya <maison.sya.it@gmail.com>
2025-07-28 22:58:12 +0200

    Add files via upload

---
+++ bijoux.js
@@ -0,0 +1,48 @@
+// Liste des bijoux exportée pour le catalogue
+const bijoux = [
+  {
+    nom: "Collier Almera",
+    description: "",
+    prix: "59",
+    promo: null,
+    images: [
+      "https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Collier%20Almera%201.png",
+      "https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Collier%20Almera%202.png"
+    ],
+    categorie: "Collier",
+    status: ""
+  },
+  {
+    nom: "Collier One Star",
+    description: "",
+    prix: "49",
+    promo: null,
+    images: ["https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Collier%20Orlane%201.png",
+        "https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Collier%20Orlane%202.png"
+          ],
+    categorie: "Collier",
+    status: ""
+  },
+  {
+    nom: "Collier Seadora",
+    description: "",
+    prix: "59",
+    promo: null,
+    images: ["https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Collier%20Seadora%201.png",
+        "https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Collier%20Seadora%202.png"
+          ],
+    categorie: "Collier",
+    status: ""
+  },
+  {
+    nom: "Boucles Seadora",
+    description: "",
+    prix: "35",
+    promo: null,
+    images: ["https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Boucles%20Seadora.png",
+      "https://raw.githubusercontent.com/MaisonSya/catalogue/refs/heads/main/img/Boucles%20Seadora%202.png"
+          ],
+    categorie: "Boucles d'oreilles",
+    status: ""
+  }
+]; 
